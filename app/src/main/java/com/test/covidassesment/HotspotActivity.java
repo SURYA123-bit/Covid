c4a850b944c51f0fc5086c11eba7a64d3a0a821e
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-05-31 09:49:18 -0400

    First Commit

---
+++ app/src/main/java/com/test/covidassesment/HotspotActivity.java
@@ -0,0 +1,14 @@
+package com.test.covidassesment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HotspotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotspot);
+    }
+}
