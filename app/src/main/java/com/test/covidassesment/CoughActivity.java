fc26ce1730655378a0b19f471372ec4a25d4ac1c
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-06-12 19:55:02 -0400

    First Commit

--- app/src/main/java/com/test/covidassesment/CoughActivity.java
+++ app/src/main/java/com/test/covidassesment/CoughActivity.java
@@ -1,14 +1,14 @@
 package com.test.covidassesment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class TravelActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CoughActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_travel);
+        setContentView(R.layout.activity_cough);
     }
 }
---
+++ app/src/main/java/com/test/covidassesment/FeverActivity.java
@@ -0,0 +1,14 @@
+package com.test.covidassesment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FeverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fever);
+    }
+}
